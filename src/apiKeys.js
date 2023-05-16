0512058145f81abcb54357fded445852e25fbd2e
Sarangverma98 <sarangverma98@gmail.com>
2023-05-16 15:30:19 +0530

    initial

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "340e7acfce6829605a7a5c6e1e4eee9d",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
